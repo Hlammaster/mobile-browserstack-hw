faf9e75cb6957da7647088a222f68e23cadda75c
Johnny Firo <hlammaster@mail.ru>
2023-05-05 17:34:33 +0300

    added BrowserStackDriver

--- src/test/java/BrowserStackTests.java
+++ src/test/java/BrowserStackTests.java
@@ -14,30 +14,6 @@ import java.util.List;
 public class BrowserStackTests {
     @Test
     void searchTest() throws MalformedURLException, InterruptedException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-
-        // Set your access credentials
-        caps.setCapability("browserstack.user", "johnnyfiro_v2Un4A");
-        caps.setCapability("browserstack.key", "MePKpRXM2KiMMu8qiWTb");
-
-        // Set URL of the application under test
-        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-
-        // Specify device and os_version for testing
-        caps.setCapability("device", "Google Pixel 3");
-        caps.setCapability("os_version", "9.0");
-
-        // Set other BrowserStack capabilities
-        caps.setCapability("project", "First Java Project");
-        caps.setCapability("build", "browserstack-build-1");
-        caps.setCapability("name", "first_test");
-
-
-        // Initialise the remote Webdriver using BrowserStack remote URL
-        // and desired capabilities defined above
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
-                new URL("http://hub.browserstack.com/wd/hub"), caps);
-//                new URL("http://hub-cloud.browserstack.com"), caps);
 
         // Test case for the BrowserStack sample Android app.
         // If you have uploaded your app, update the test case here.
