b47ae286a049bc91196cdd3deb717e772972bfff
Johnny Firo <hlammaster@mail.ru>
2023-05-05 19:14:34 +0300

    added Allure

--- src/test/java/BrowserStackTests.java
+++ src/test/java/BrowserStackTests.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
@@ -7,14 +8,20 @@ import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 
 public class BrowserStackTests extends TestBase {
     @Test
+    @DisplayName("Successful searh")
     void searchTest() throws MalformedURLException, InterruptedException {
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        step("Type search", () -> {
+                    $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+                    $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+                });
+        step("Verify content found", () -> {
         $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container"))
                 .shouldHave(CollectionCondition.sizeGreaterThan(0));
+        });
     }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,16 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import config.BrowserStackDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
     @BeforeAll
@@ -17,11 +20,15 @@ public class TestBase {
     }
     @BeforeEach
     void  addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open();
     }
 
     @AfterEach
     void afterEach(){
+        String sessionId = sessionId().toString();
+        Attach.pageSource();
         closeWebDriver();
+        Attach.addVideo(sessionId);
     }
 }
